ee542080886814bfc27e15965a61312bf8571aab
alvaro <alvarocliment1999@gmail.com>
2023-11-07 16:39:58 +0100

    commit intentando solucionar problema injectable

--- src/app/users/users.model.ts
+++ src/app/users/users.model.ts
@@ -1,2 +1,20 @@
+export interface IAdress{
+  city: string;
+  street: string;
+  number: number;
+  zipcode: string;
+}
+export interface IName{
+  firstname: string;
+  lastname: string;
+}
+
 export interface IUsers {
+  address: IAdress;
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  name: IName;
+  phone: string;
 }
