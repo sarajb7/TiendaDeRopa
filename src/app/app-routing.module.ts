802a2a81e6959c18f0575ce27b7550ddfb2ce8c8
Sara Jimenez <sarajb0710@gmail.com>
2023-11-13 00:33:03 +0000

    commit sara final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home/home.component';
 import { ProductsListComponent } from './products/products-list/products-list.component';
 import { UsersFormComponent } from './users/users-form/users-form.component';
 import { ProductsFormComponent } from './products/products-form/products-form.component';
+import { ProductsDetailComponent } from './products/products-detail/products-detail.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -12,6 +13,11 @@ const routes: Routes = [
     path: 'products/new',
     component: ProductsFormComponent
   },
+  {
+    path: 'products/:id',
+    component: ProductsDetailComponent
+  },
+  
   {path: 'products', component: ProductsListComponent},
   {path: 'users', component: UsersFormComponent},
   {path:'', redirectTo: '/home', pathMatch: 'full'},
